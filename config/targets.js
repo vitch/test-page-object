4455099aa52208a259ce8cebfc8f5b8ca3907424
Kelvin Luck <kelvin.luck@crowdstrike.com>
2018-04-24 17:32:28 +0100

    Match targets

--- config/targets.js
+++ config/targets.js
@@ -1,9 +1,9 @@
 /* eslint-env node */
 module.exports = {
   browsers: [
-    'ie 9',
-    'last 1 Chrome versions',
-    'last 1 Firefox versions',
-    'last 1 Safari versions'
+    'ie 11',
+    'last 3 Edge versions',
+    'last 3 Chrome versions',
+    'last 3 Firefox versions',
   ]
 };
