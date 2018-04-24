3a595857593c3b0ff9e613e987930c3beafe32a2
Kelvin Luck <kelvin.luck@crowdstrike.com>
2018-04-24 16:11:57 +0100

    Updated test-helper and config to match `ember-qunit` requirements

--- config/environment.js
+++ config/environment.js
@@ -40,6 +40,8 @@ module.exports = function(environment) {
     ENV.APP.LOG_ACTIVE_GENERATION = false;
     ENV.APP.LOG_VIEW_LOOKUPS = false;
 
+    ENV.APP.autoboot = false;
+
     ENV.APP.rootElement = '#ember-testing';
   }
 
